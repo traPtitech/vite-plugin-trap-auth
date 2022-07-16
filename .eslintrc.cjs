944d8d4b2ed2a0a03bf144940a614d7c67840ff3
sapphi-red <green@sapphi.red>
2022-07-16 23:24:27 +0900

    remove eslint-plugin-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    'prettier'
   ],
   rules: {
     'no-console': 'warn',
